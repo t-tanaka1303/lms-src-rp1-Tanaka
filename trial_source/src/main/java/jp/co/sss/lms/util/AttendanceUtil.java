7e3877291b40b5a4db9116969f8362c162aea630
edu <edu@192.168.0.147>
2025-12-23 18:01:11 +0900

    rp:12/23進捗　task26 util・htmlの編集
    ・html編集中にエラーが出たので（おそらくform関連）明日対応

--- trial_source/src/main/java/jp/co/sss/lms/util/AttendanceUtil.java
+++ trial_source/src/main/java/jp/co/sss/lms/util/AttendanceUtil.java
@@ -1,6 +1,8 @@
 package jp.co.sss.lms.util;
 
 import java.text.ParseException;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.LinkedHashMap;
 
@@ -25,37 +27,42 @@ public class AttendanceUtil {
 
 	/**
 	 * SSS定時・出退勤時間を元に、遅刻早退を判定をする
+	 * 
+	 * @param trainingStartTime 開始時刻
+	 * @param trainingEndTime   終了時刻
+	 * @return 遅刻早退を判定メソッド
 	 */
-	public AttendanceStatusEnum getStatus(
-			TrainingTime trainingStartTime,
+	public AttendanceStatusEnum getStatus(TrainingTime trainingStartTime,
 			TrainingTime trainingEndTime) {
-		return getStatus(
-				trainingStartTime,
-				trainingEndTime,
-				Constants.SSS_WORK_START_TIME,
+		return getStatus(trainingStartTime, trainingEndTime, Constants.SSS_WORK_START_TIME,
 				Constants.SSS_WORK_END_TIME);
 	}
 
-	private AttendanceStatusEnum getStatus(
-			TrainingTime trainingStartTime,
-			TrainingTime trainingEndTime,
-			TrainingTime workStartTime,
-			TrainingTime workEndTime) {
-
-		if (workStartTime == null || workStartTime.isBlank()
-				|| workEndTime == null || workEndTime.isBlank()) {
+	/**
+	 * 与えられた定時・出退勤時間を元に、遅刻早退を判定する
+	 * 
+	 * @param trainingStartTime 開始時刻
+	 * @param trainingEndTime   終了時刻
+	 * @param workStartTime     定時開始時刻
+	 * @param workEndTime       定時終了時刻
+	 * @return 判定結果
+	 */
+	private AttendanceStatusEnum getStatus(TrainingTime trainingStartTime,
+			TrainingTime trainingEndTime, TrainingTime workStartTime, TrainingTime workEndTime) {
+		// 定時が不明な場合、NONEを返却する
+		if (workStartTime == null || workStartTime.isBlank() || workEndTime == null
+				|| workEndTime.isBlank()) {
 			return AttendanceStatusEnum.NONE;
 		}
-
 		boolean isLate = false, isEarly = false;
-
+		// 定時より1分以上遅く出社していたら遅刻(＝はセーフ)
 		if (trainingStartTime != null && trainingStartTime.isNotBlank()) {
 			isLate = (trainingStartTime.compareTo(workStartTime) > 0);
 		}
+		// 定時より1分以上早く退社していたら早退(＝はセーフ)
 		if (trainingEndTime != null && trainingEndTime.isNotBlank()) {
 			isEarly = (trainingEndTime.compareTo(workEndTime) < 0);
 		}
-
 		if (isLate && isEarly) {
 			return AttendanceStatusEnum.TARDY_AND_LEAVING_EARLY;
 		}
@@ -70,38 +77,49 @@ public class AttendanceUtil {
 
 	/**
 	 * 中抜け時間を時(hour)と分(minute)に変換
+	 *
+	 * @param min 中抜け時間
+	 * @return 時(hour)と分(minute)に変換したクラス
 	 */
 	public TrainingTime calcBlankTime(int min) {
 		int hour = min / 60;
 		int minute = min % 60;
-		return new TrainingTime(hour, minute);
+		TrainingTime total = new TrainingTime(hour, minute);
+		return total;
 	}
 
 	/**
 	 * 時刻分を丸めた本日日付を取得
+	 * 
+	 * @return "yyyy/M/d"形式の日付
 	 */
 	public Date getTrainingDate() {
+		Date trainingDate;
 		try {
-			return dateUtil.parse(dateUtil.toString(new Date()));
+			trainingDate = dateUtil.parse(dateUtil.toString(new Date()));
 		} catch (ParseException e) {
+			// DateUtil#toStringとparseは同様のフォーマットを使用しているため、起こりえないエラー
 			throw new IllegalStateException();
 		}
+		return trainingDate;
 	}
 
 	/**
 	 * 休憩時間取得
+	 * 
+	 * @return 休憩時間
 	 */
 	public LinkedHashMap<Integer, String> setBlankTime() {
 		LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
 		map.put(null, "");
-
-		for (int i = 15; i < 480; i += 15) {
+		for (int i = 15; i < 480;) {
 			int hour = i / 60;
 			int minute = i % 60;
-
 			String time;
+
 			if (hour == 0) {
 				time = minute + "分";
+
 			} else if (minute == 0) {
 				time = hour + "時間";
 			} else {
@@ -109,42 +127,113 @@ public class AttendanceUtil {
 			}
 
 			map.put(i, time);
+
+			i = i + 15;
+
 		}
 		return map;
 	}
+
+	/**
+	 *  task26追加 時間取得
+	 * 
+	 * @return 時間
+	 */
+	public LinkedHashMap<Integer, String> setTrainingTimeHh() {
+		LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+		map.put(null, "");
+		for (int i = 0; i < 24; i++) {
+
+			if (i < 10) {
+				map.put(i, "0" + i);
+			} else {
+				map.put(i, String.valueOf(i));
+			}
+		}
+		return map;
+	}
+
+	/**
+		 * task26 分取得
+		 * 
+		 * @return 分
+		 */
+	public LinkedHashMap<Integer, String> setTrainingTimeMi() {
+		LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
+		map.put(null, "");
+		for (int i = 0; i < 60; i++) {
+			if (i < 10) {
+				map.put(i, "0" + i);
+			} else {
+				map.put(i, String.valueOf(i));
+			}
+		}
+		return map;
+	} //ここまでtask26
 
 	/**
 	 * 研修日の判定
+	 * 
+	 * @param courseId
+	 * @param trainingDate
+	 * @return 判定結果
 	 */
 	public boolean isWorkDay(Integer courseId, Date trainingDate) {
-		Integer count =
-				mSectionMapper.getSectionCountByCourseId(courseId, trainingDate);
-		return count > 0;
-	}
-
-	// ==================================================
-	// task26: 出勤・退勤時間「時・分」プルダウン用
-	// ==================================================
-
-	/**
-	 * 時プルダウン用マップ（0～23）
-	 */
-	public LinkedHashMap<Integer, String> getHourMap() {
-		LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
-		for (int i = 0; i <= 23; i++) {
-			map.put(i, String.format("%02d", i));
+		Integer count = mSectionMapper.getSectionCountByCourseId(courseId, trainingDate);
+		if (count > 0) {
+			return true;
 		}
-		return map;
+		return false;
 	}
 
 	/**
-	 * 分プルダウン用マップ（0～59）
+	 * task26追加 開始時刻(hh;mm)を時(hh)に分割
+	 * 
+	 * @return 時
 	 */
-	public LinkedHashMap<Integer, String> getMinuteMap() {
-		LinkedHashMap<Integer, String> map = new LinkedHashMap<>();
-		for (int i = 0; i <= 59; i++) {
-			map.put(i, String.format("%02d", i));
-		}
-		return map;
+
+	public Integer getTrainingHour(String trainingStartTime) {
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+		LocalTime time = LocalTime.parse(trainingStartTime, formatter);
+		Integer integerHour = time.getHour();
+
+		return integerHour;
+	}
+
+	/**
+	 * task26 開始時刻(hh;mm)を分(mm)に分割
+	 * 
+	 * @return 分
+	 */
+
+	public Integer getTrainingMinute(String trainingStartTime) {
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+		LocalTime time = LocalTime.parse(trainingStartTime, formatter);
+		Integer integerMinute = time.getMinute();
+
+		return integerMinute;
+
+	}
+	// Integer → String 変換
+	public String getIntegerHour(Integer integerHour) {
+	    return integerHour == null ? "" : String.valueOf(integerHour);
+	}
+
+	public String getIntegerMinute(Integer integerMinute) {
+	    return integerMinute == null ? "" : String.valueOf(integerMinute);
 	}
 }
+	/**public String getIntegerHour(Integer integerHour) {
+
+		String Hour = String.valueOf(integerHour);
+		return Hour;
+	}
+
+	public String getIntegerMinute(Integer integerMinute) {
+
+		String Minute = String.valueOf(integerMinute);
+		return Minute;
+	}*/
+
